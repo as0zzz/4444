60b91bf946e6736884f2e252066942028c87bb5a
maximus-go-1 <max_maximus_go_1@mail.ru>
2026-04-10 22:28:42 +0300

    Update urls.py

--- IT_mentor_site_test/blog/urls.py
+++ IT_mentor_site_test/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.open_1, name='open_1'),
@@ -28,4 +30,6 @@ urlpatterns = [
     path('profile/view/', views.view_profile, name='view_profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('event/create/', views.create_event, name='create_event'),
-]
+    path('event/<int:event_id>/delete/', views.delete_event, name='delete_event'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
